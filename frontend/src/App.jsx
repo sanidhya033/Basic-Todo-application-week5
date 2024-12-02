7940d1062165d6ba4acadbe4070ee5d3c80ee429
sanidhya033 <155379841+sanidhya033@users.noreply.github.com>
2024-12-02 07:55:59 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { CreateTodo } from './components/Createtodo'
-import { RenderAllTodo } from './components/Todos'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { CreateTodo } from "./components/Createtodo";
+import { RenderAllTodo } from "./components/Todos";
 
 function App() {
-  const [todos, settodos] = useState([]);
-  fetch(" http://localhost:3000/todo")
-  .then(async function (res) {
-    const json =await res.json;
+  const [todos, setTodos] = useState([]);
 
-    settodos(json.todos);
-  })
+  // Fetch todos when the component mounts
+  useEffect(() => {
+    fetch("http://localhost:3000/todo")
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data.todos);
+      })
+      .catch((error) => {
+        console.error("Error fetching todos:", error);
+      });
+  }, []);
+
+  // Function to handle completed todos
+  const handleTodoUpdate = (updatedTodo) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === updatedTodo.id ? updatedTodo : todo
+      )
+    );
+  };
+
+  // Function to handle adding a new todo
+  const handleAddTodo = (newTodo) => {
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  };
 
   return (
     <div>
-       <CreateTodo></CreateTodo>
-       <RenderAllTodo todos={[
-          {
-            title:"go to gym",
-            description:"Stay healthy",
-            completed:false
-          }
-        ]}></RenderAllTodo>
+      <CreateTodo onAddTodo={handleAddTodo} />
+      <RenderAllTodo todos={todos} onUpdate={handleTodoUpdate} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
